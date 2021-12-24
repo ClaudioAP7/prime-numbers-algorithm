c4d69a5007354f19de4df7137e2df7117039128e
Claudio <cpichico@externos.bancoestado.cl>
2021-12-24 11:23:58 -0300

    SET unit test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -11,15 +11,12 @@ const ERROR_CASES_MOCK = {
 }
 const GENERATE_PRIME_MOCK = [97,89,83,79,73,71,67,61,59,53,47,43,41,37,31,29,23,19,17,13,11,7,5,3,2]
 
-beforeAll(async () => {
-})
-
 afterAll(() => {
     server.close()
 })
 
-beforeEach(() => {
-    jest.restoreAllMocks()
+afterEach(() => {
+    jest.resetAllMocks()
 })
 
 describe("GET Prime numbers", () => {
